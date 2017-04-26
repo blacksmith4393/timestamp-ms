7ace87d2c18698c794b0a4393df02773c5968c78
Zach <zach.smith33@gmail.com>
2017-04-26 09:56:40 -0400

    modified formatting of timestamp module. add styleing to index.html

--- timestamp.js
+++ timestamp.js
@@ -1,8 +1,20 @@
 
 module.exports = function(str){
-  let d = new Date(str);
-  let date = {};
-  date.unix = d.getTime();
-  date.natural = d.toDateString();
+    let date = {};
+
+  if(parseInt(str)){
+    let d = new Date(parseInt(str)*1000);
+    date.unix = (d/1000);
+    date.natural = d.toDateString();
+  } else if(!parseInt(str)){
+    let d = new Date(str);
+    date.unix = (d.getTime()) / 1000;
+    if(!date.unix){
+      date.natural = null;
+    } else {
+      date.natural = d.toDateString();
+    }
+  }
+  
   return date;
 };
