3fb94750210a461e241c8267483125ee9ff853c1
Zach <zach.smith33@gmail.com>
2017-04-26 09:16:10 -0400

    edit timestamp module and route handler. add index.html landing page

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app =  express();
 const timestamp = require('./timestamp');
 
 const port = 5000;
+
 app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/:str', (req, res) => {
   
   let date = timestamp(req.params.str);
   res.json(date);
-});
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
 });
 
 app.listen(port, function () {
@@ -23,6 +23,7 @@ app.listen(port, function () {
 app.use(function (req, res, next) {
   res.status(404).send("Sorry can't find that!");
 });
+
 app.use(function (err, req, res, next) {
   console.error(err.stack);
   res.status(500).send('Something broke!');
--- timestamp.js
+++ timestamp.js
@@ -1,18 +1,8 @@
-function unixTime(date){
-  return date.getTime();
-}
-function parseTime(date){
-  return {
-    hour: date.getHours(),
-    minute: date.getMinutes(),
-    second: date.getSeconds(),
-  };
-}
 
 module.exports = function(str){
   let d = new Date(str);
   let date = {};
-  date.unix = unixTime(d);
+  date.unix = d.getTime();
   date.natural = d.toDateString();
   return date;
 };
